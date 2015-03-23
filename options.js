768f8d6f89acc5d73b230c38e26f93f81dcad9b7
unknown <tomoki_ishizawa@20140926NB001P.ad-leg.local>
2015-03-23 09:59:23 +0900

    when you save, it show message "done"

--- options.js
+++ options.js
@@ -4,6 +4,7 @@ $(function(){
   // ローカルストレージに保存する。
   $("#save").click(function () {
     localStorage["URL"] = $("#URL").val();
+    alert($("#URL").val()+"で値が保存されました。");
   });
 
   // オプション画面の初期値を設定する
